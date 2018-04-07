7c229b227ab21b340bfa9767b659670807db943e
aditya21 <aditya21.harshey@gmail.com>
2018-04-08 01:19:07 +0530

    second commit

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import React from "react";
-import App from "./app";
+import React, {Component} from 'react';
+import { AppRegistry, AsyncStorage, View } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import Login from './components/login';
+import { persistStore } from 'redux-persist';
+import { Spinner } from 'native-base';
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import { registerScreens } from './screens';
+import { Navigation } from 'react-native-navigation';
 
-export default class App1 extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isReady: false
-    };
-  }
+const store = configureStore();
 
-  render() {
-    return (
-        <App />
-    );
-  }
-}
+registerScreens(store, Provider);
+
+Navigation.startSingleScreenApp({
+          screen: {
+            screen: 'app.Login', 
+            title: 'Welcome', 
+            navigatorStyle: {}, 
+            navigatorButtons: {} 
+            },
+            drawer: {},
+            passProps: {}, // simple serializable object that will pass as props to all top screens (optional)
+            animationType: 'slide-down' 
+        });
